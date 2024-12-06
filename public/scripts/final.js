e1e14512947c60d22ff7f0c1645a5f7a64f77b06
Bosco-13 <uft24kuu@uea.ac.uk>
2024-12-06 21:21:25 +0000

    fetch of the newsletter

--- public/scripts/final.js
+++ public/scripts/final.js
@@ -1,12 +1,27 @@
 const localJsonFile = "final.json";
-const currentPage = document.querySelector("body").id;
+// get the id of the body element to get the current page
+currentPage = document.getElementsByTagName("body").id;
+console.log(currentPage);
 document.addEventListener("DOMContentLoaded", ()=>{
     fetch(localJsonFile)
-    .then(resopnse => response.json())
+    .then(response => response.json())
     .then(resopnseData =>{
         // sign up page fetch
         if (currentPage = "signUp"){
-
+            const data = resopnseData.newsletter;
+            console.log(data);
+            header = document.getElementById("newsletter-header").children;
+            header[0].textContent = data.header.title;
+            header[1].textContent = data.header.description;
+            formElement = document.getElementById("signup-form").children;
+            for(let i = 0; i < 4; i++){
+                formElement[i].textContent = data.inputs[i].label;
+                inputElement = document.createElement("input");
+                inputElement.type = data.inputs[i].type;
+                inputElement.name = data.inputs[i].name;
+                formElement[i].textContent = data.inputs[i].label;
+                formElement[i].appendChild(inputElement);
+            }
         }
         // index page fetch
         if (currentPage = "homepage"){
