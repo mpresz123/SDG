a0a5df302dcd86f884bc0a1b52e703e08b2e20d0
Bosco-13 <uft24kuu@uea.ac.uk>
2024-12-08 20:37:12 +0000

    zH.js merged to final.js

--- public/scripts/final.js
+++ public/scripts/final.js
@@ -332,7 +332,56 @@ document.addEventListener("DOMContentLoaded", () => {
       if ((currentPage === "/goodhealth.html")) {
       }
       // zero hunger page fetch
-      if ((currentPage === "/zH.html")) {
+      if ((currentPage === "zH.html")) {
+        const data = responseData.zeroHunger;
+        // adding the heading element 
+        headerFigure = document.getElementById("headerLeft");
+        img = document.createElement("img");
+        img.src = data.header.imgURL;
+        headerFigure.appendChild(img);
+        headerContent = document.getElementById("headerRight").children;
+        for (i = 0; i < 3; i++){
+          headerContent[i].textContent = data.header.rightContent[i];
+        }
+
+      //   adding the target element
+        targetTitle = document.getElementById("targetTitle");
+        targetTitle.textContent = data.targets.title;
+        //get each target box
+        tList = document.querySelectorAll(".target");
+        // loop through each box and load contents
+        for (i = 0; i < tList.length; i++){
+          // create a list of the content elements of the box
+          targetElement = tList[i].children;
+          // h3
+          targetElement[0].textContent = data.targets.items[i].Title;
+          // p
+          targetElement[1].textContent = data.targets.items[i].Target;
+          //img create
+          image = document.createElement("img");
+          image.src = data.targets.items[i].imgurl;
+          image.alt = data.targets.items[i].source;
+          //append img into figure
+          targetElement[2].appendChild(image);
+        }
+        // plans
+        document.getElementById("plan").children[0].textContent = data.plans.title;
+        planList = document.getElementsByClassName("planListBox");
+        for (i = 0; i < planList.length; i++){
+          boxContent = planList[i].children;
+          boxContent[0].textContent = i+1;
+          boxContent[1].textContent = data.plans.items[i].Title;
+          boxContent[2].textContent = data.plans.items[i].Plan;
+        }
+        // plans
+        document.getElementById("policy").children[0].textContent = data.policy.title;
+        policyList = document.getElementsByClassName("policyListBox");
+        for (i = 0; i < policyList.length; i++){
+          boxContent = policyList[i].children;
+          boxContent[0].textContent = i+1;
+          boxContent[1].textContent = data.policy.items[i].Title;
+          boxContent[2].textContent = data.policy.items[i].Plan;
+        }
       }
 
       /*global footer*/
