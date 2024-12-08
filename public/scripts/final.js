2b2501b41af2e82f3fdaf0c55e60c2d706ef0722
mpresz123 <mikolajpresz@gmail.com>
2024-12-08 21:17:08 +0000

    fixed homepage js, sorted all js into if statement

--- public/scripts/final.js
+++ public/scripts/final.js
@@ -3,223 +3,6 @@ var localJSONFile = "final.json";
 
 const currentPage = window.location.pathname.split("/").pop();
 
-var featuredgoals = document.querySelector("#home-featured-goals");
-var overlayclass = document.querySelector(".overlay");
-var article1 = document.querySelector("#home-article-1");
-var article2 = document.querySelector("#home-article-2");
-var article3 = document.querySelector("#home-article-3");
-var article6 = document.querySelector("#team");
-var ourteam = document.querySelector("#team-section");
-var article4 = document.querySelector("#team-contributions-article");
-var article5 = document.querySelector("#team-article");
-
-//team javascript
-
-if (document.getElementById("teampage")) {
-    document.addEventListener('DOMContentLoaded', () => {
-        fetch(localJSONFile)
-            .then(response => response.json())
-            .then(responseData => {
-                const teamData = responseData.ourteam;
-
-              
-
-                const heading1 = document.createElement("h1");
-                heading1.textContent = teamData.main_heading;
-                heading1.setAttribute("id", "home-main-title");
-
-                const heading2 = document.createElement("h1");
-                heading2.setAttribute("id", "team-heading");
-                heading2.textContent = teamData.team_heading;
-
-                const heading3 = document.createElement("h1");
-                heading3.setAttribute("id", "contributions-heading");
-                heading3.textContent = teamData.contributions;
-
-                const button1 = document.createElement("button");
-                button1.setAttribute("class", "trello-button");
-                button1.textContent = teamData.button1;
-                button1.onclick = () => {
-                    window.location.href = "https://trello.com/b/ceSVFrIr/web-based-project";
-                };
-
-                const text1 = document.createElement("p")
-                text1.textContent = teamData.bio1;
-                const text2 = document.createElement("p")
-                text2.textContent = teamData.bio2;
-                const text3 = document.createElement("p")
-                text3.textContent = teamData.bio3;
-                const text4 = document.createElement("p")
-                text4.textContent = teamData.bio4;
-
-                
-
-
-                ourteam.appendChild(article4);
-                article4.appendChild(heading3);
-                article4.appendChild(button1);
-                ourteam.appendChild(article5);
-                article4.appendChild(heading1);
-                ourteam.appendChild(heading1);
-
-                article5.appendChild(heading2);
-                article5.appendChild(text1);
-                article5.appendChild(text2);
-                article5.appendChild(text3);
-                article5.appendChild(text4);
-
-            })
-            .catch(error => console.error('Error loading JSON data:', error));
-    });
-}
-
-//home javascript
-
-if (document.getElementById("homepage")) {
-    document.addEventListener('DOMContentLoaded', () => {
-        fetch(localJSONFile)
-            .then(response => response.json())
-            .then(responseData => {
-                // Access the 'home' object directly
-                const homeData = responseData.home;
-                const lineBreak = document.createElement("br");
-                const lineBreak2 = document.createElement("br");
-                const lineBreak3 = document.createElement("br");
-                const lineBreak4 = document.createElement("br");
-                const lineBreak5 = document.createElement("br");
-                const lineBreak6 = document.createElement("br");
-                const lineBreak7 = document.createElement("br");
-
-
-
-                const schedules_section = document.createElement("section");
-                schedules_section.setAttribute("id", "home-featured-goals");
-
-                // Create the heading for the main title
-                const heading1 = document.createElement("h1");
-                heading1.setAttribute("id", "home-main-title");
-                heading1.textContent = homeData.main_title;
-
-                const mainheading1 = document.createElement("h1");
-                mainheading1.textContent = homeData.mainheading1;
-                mainheading1.setAttribute("id", "home-h1");
-
-                const mainheading2 = document.createElement("h1");
-                mainheading2.textContent = homeData.mainheading2;
-                mainheading2.setAttribute("id", "home-h2");
-
-                const check_team = document.createElement("h4");
-                check_team.textContent = homeData.team_heading;
-                
-
-                // Create the image element
-                const imageElement = document.createElement("img");
-                imageElement.src = homeData.imageURL;
-                imageElement.alt = homeData.alt;
-                imageElement.setAttribute("id", "home-info-pic");
-
-                const heading4 = document.createElement("h4");
-                heading4.textContent = homeData.title;
-
-                const text1 = document.createElement("p");
-                text1.textContent = homeData.text1;
-
-                const button = document.createElement("button");
-                button.setAttribute("id", "home-info-button");
-                button.textContent = homeData.button;
-                button.onclick = () => {
-                  window.location.href = "/noPov.html";
-              };
-
-
-                const button2 = document.createElement("button");
-                button2.setAttribute("id", "home-info-button");
-                button2.textContent = homeData.button;
-                button2.onclick = () => {
-                  window.location.href = "/zH.html";
-              };
-
-               const team_button = document.createElement("button");
-               team_button.setAttribute("id", "home-info-button");
-               team_button.textContent = homeData.button3;
-               team_button.onclick = () => {
-                window.location.href = "/ourteam.html";
-            };
-              
-
-                const heading4a = document.createElement("h4");
-                heading4a.textContent = homeData.title2;
-
-                const imageElementa = document.createElement("img");
-                imageElementa.src = homeData.imageURL2;
-                imageElementa.alt = homeData.alt;
-                imageElementa.setAttribute("id", "home-info-pic")
-
-                const text2 = document.createElement("p");
-                text2.textContent = homeData.text2;
-
-                const imageElementb = document.createElement("img");
-                imageElementb.src = homeData.imageURL3;
-                imageElementb.alt = homeData.alt;
-                imageElementb.setAttribute("id", "home-info-pic")
-
-                const button3 = document.createElement("button");
-                button3.setAttribute("id", "home-info-button");
-                button3.textContent = homeData.button;
-                button3.onclick = () => {
-                  window.location.href = "/goodhealth.html";
-              };
-
-                const text3 = document.createElement("p");
-                text3.textContent = homeData.text3;
-
-                const heading4b = document.createElement("h4")
-                heading4b.textContent = homeData.title3;
-
-
-                schedules_section.appendChild(heading1);
-                overlayclass.appendChild(mainheading1);
-                overlayclass.appendChild(mainheading2);
-
-                schedules_section.appendChild(article1);
-                schedules_section.appendChild(article2);
-                schedules_section.appendChild(article3);
-
-
-                article1.appendChild(imageElement);
-                article1.appendChild(heading4);
-                article1.appendChild(lineBreak);
-                article1.appendChild(text1);
-                article1.appendChild(lineBreak2);
-                article1.appendChild(button);
-
-                article2.appendChild(imageElementa);
-                article2.appendChild(heading4a);
-                article2.appendChild(lineBreak3);
-                article2.appendChild(text2);
-                article2.appendChild(lineBreak4);
-                article2.appendChild(button2);
-
-                article3.appendChild(imageElementb);
-                article3.appendChild(heading4b);
-                article3.appendChild(lineBreak5);
-                article3.appendChild(text3);
-                article3.appendChild(lineBreak6);
-                article3.appendChild(button3);
-
-                article6.appendChild(check_team);
-                article6.appendChild(lineBreak7);
-                article6.appendChild(team_button);
-
-
-                featuredgoals.appendChild(schedules_section);
-            })
-            .catch(error => console.error('Error loading JSON data:', error));
-    });
-}
-
-//global nav bar and footer
-
 document.addEventListener("DOMContentLoaded", () => {
   fetch(localJsonFile)
     .then((response) => response.json())
@@ -253,6 +36,163 @@ document.addEventListener("DOMContentLoaded", () => {
         navLinks.appendChild(li);
       });
 
+      // home page fetch
+      if(currentPage === "index.html"){
+
+        // Get references to existing DOM elements
+        const featuredgoals = document.querySelector("#home-featured-goals");
+        const overlayclass = document.querySelector(".overlay");
+        
+        // Create article elements
+        const article1 = document.createElement("article");
+        article1.id = "home-article-1";
+        
+        const article2 = document.createElement("article");
+        article2.id = "home-article-2";
+        
+        const article3 = document.createElement("article");
+        article3.id = "home-article-3";
+        
+        const article6 = document.createElement("article");
+        article6.id = "team";
+
+        // Access the 'home' object directly
+        const homeData = responseData.home;
+        const lineBreak = document.createElement("br");
+        const lineBreak2 = document.createElement("br");
+        const lineBreak3 = document.createElement("br");
+        const lineBreak4 = document.createElement("br");
+        const lineBreak5 = document.createElement("br");
+        const lineBreak6 = document.createElement("br");
+        const lineBreak7 = document.createElement("br");
+
+        const schedules_section = document.createElement("section");
+        schedules_section.setAttribute("id", "home-featured-goals");
+
+        // Create the heading for the main title
+        const heading1 = document.createElement("h1");
+        heading1.setAttribute("id", "home-main-title");
+        heading1.textContent = homeData.main_title;
+
+        const mainheading1 = document.createElement("h1");
+        mainheading1.textContent = homeData.mainheading1;
+        mainheading1.setAttribute("id", "home-h1");
+
+        const mainheading2 = document.createElement("h1");
+        mainheading2.textContent = homeData.mainheading2;
+        mainheading2.setAttribute("id", "home-h2");
+
+        const check_team = document.createElement("h4");
+        check_team.textContent = homeData.team_heading;
+        
+
+        // Create the image element
+        const imageElement = document.createElement("img");
+        imageElement.src = homeData.imageURL;
+        imageElement.alt = homeData.alt;
+        imageElement.setAttribute("id", "home-info-pic");
+
+        const heading4 = document.createElement("h4");
+        heading4.textContent = homeData.title;
+
+        const text1 = document.createElement("p");
+        text1.textContent = homeData.text1;
+
+        const button = document.createElement("button");
+        button.setAttribute("id", "home-info-button");
+        button.textContent = homeData.button;
+        button.onclick = () => {
+          window.location.href = "/noPov.html";
+        };
+
+
+        const button2 = document.createElement("button");
+        button2.setAttribute("id", "home-info-button");
+        button2.textContent = homeData.button;
+        button2.onclick = () => {
+          window.location.href = "/zH.html";
+        };
+
+       const team_button = document.createElement("button");
+       team_button.setAttribute("id", "home-info-button");
+       team_button.textContent = homeData.button3;
+       team_button.onclick = () => {
+          window.location.href = "/ourteam.html";
+        };
+      
+
+        const heading4a = document.createElement("h4");
+        heading4a.textContent = homeData.title2;
+
+        const imageElementa = document.createElement("img");
+        imageElementa.src = homeData.imageURL2;
+        imageElementa.alt = homeData.alt;
+        imageElementa.setAttribute("id", "home-info-pic")
+
+        const text2 = document.createElement("p");
+        text2.textContent = homeData.text2;
+
+        const imageElementb = document.createElement("img");
+        imageElementb.src = homeData.imageURL3;
+        imageElementb.alt = homeData.alt;
+        imageElementb.setAttribute("id", "home-info-pic")
+
+        const button3 = document.createElement("button");
+        button3.setAttribute("id", "home-info-button");
+        button3.textContent = homeData.button;
+        button3.onclick = () => {
+          window.location.href = "/goodhealth.html";
+        };
+
+        const text3 = document.createElement("p");
+        text3.textContent = homeData.text3;
+
+        const heading4b = document.createElement("h4")
+        heading4b.textContent = homeData.title3;
+
+
+        schedules_section.appendChild(heading1);
+        
+        if (overlayclass) {
+          overlayclass.appendChild(mainheading1);
+          overlayclass.appendChild(mainheading2);
+        }
+
+        schedules_section.appendChild(article1);
+        schedules_section.appendChild(article2);
+        schedules_section.appendChild(article3);
+
+
+        article1.appendChild(imageElement);
+        article1.appendChild(heading4);
+        article1.appendChild(lineBreak);
+        article1.appendChild(text1);
+        article1.appendChild(lineBreak2);
+        article1.appendChild(button);
+
+        article2.appendChild(imageElementa);
+        article2.appendChild(heading4a);
+        article2.appendChild(lineBreak3);
+        article2.appendChild(text2);
+        article2.appendChild(lineBreak4);
+        article2.appendChild(button2);
+
+        article3.appendChild(imageElementb);
+        article3.appendChild(heading4b);
+        article3.appendChild(lineBreak5);
+        article3.appendChild(text3);
+        article3.appendChild(lineBreak6);
+        article3.appendChild(button3);
+
+        article6.appendChild(check_team);
+        article6.appendChild(lineBreak7);
+        article6.appendChild(team_button);
+
+        if (featuredgoals) {
+          featuredgoals.appendChild(schedules_section);
+          featuredgoals.appendChild(article6);
+        }
+      }
       // signup page fetch
       if (currentPage === "newsletter.html") {
         const data = responseData.newsletter;
@@ -335,7 +275,64 @@ document.addEventListener("DOMContentLoaded", () => {
         signupButton.href = data.noPov.signup.buttonUrl;
       }
       // noPov page fetch
-      if ((currentPage === "/noPov.html")) {
+      if ((currentPage === "/ourteam.html")) {
+        var featuredgoals = document.querySelector("#home-featured-goals");
+        var overlayclass = document.querySelector(".overlay");
+        var article1 = document.querySelector("#home-article-1");
+        var article2 = document.querySelector("#home-article-2");
+        var article3 = document.querySelector("#home-article-3");
+        var article6 = document.querySelector("#team");
+        var ourteam = document.querySelector("#team-section");
+        var article4 = document.querySelector("#team-contributions-article");
+        var article5 = document.querySelector("#team-article");
+      
+        const teamData = responseData.ourteam;
+
+      
+
+        const heading1 = document.createElement("h1");
+        heading1.textContent = teamData.main_heading;
+        heading1.setAttribute("id", "home-main-title");
+
+        const heading2 = document.createElement("h1");
+        heading2.setAttribute("id", "team-heading");
+        heading2.textContent = teamData.team_heading;
+
+        const heading3 = document.createElement("h1");
+        heading3.setAttribute("id", "contributions-heading");
+        heading3.textContent = teamData.contributions;
+
+        const button1 = document.createElement("button");
+        button1.setAttribute("class", "trello-button");
+        button1.textContent = teamData.button1;
+        button1.onclick = () => {
+            window.location.href = "https://trello.com/b/ceSVFrIr/web-based-project";
+        };
+
+        const text1 = document.createElement("p")
+        text1.textContent = teamData.bio1;
+        const text2 = document.createElement("p")
+        text2.textContent = teamData.bio2;
+        const text3 = document.createElement("p")
+        text3.textContent = teamData.bio3;
+        const text4 = document.createElement("p")
+        text4.textContent = teamData.bio4;
+
+        
+
+
+        ourteam.appendChild(article4);
+        article4.appendChild(heading3);
+        article4.appendChild(button1);
+        ourteam.appendChild(article5);
+        article4.appendChild(heading1);
+        ourteam.appendChild(heading1);
+
+        article5.appendChild(heading2);
+        article5.appendChild(text1);
+        article5.appendChild(text2);
+        article5.appendChild(text3);
+        article5.appendChild(text4);
       }
       // good health page fetch
       if ((currentPage === "/goodhealth.html")) {
